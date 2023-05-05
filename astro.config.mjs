5ff8070180837fcf3a8b1baa3a2e973fcc9b003c
JUan Andrade <jfandradea@gmail.com>
2023-05-04 21:03:28 -0500

    set up with tailwind and sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import sanity from "astro-sanity";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sanity(
+    {
+      projectId: '4778um7r',
+      dataset: 'production',
+      apiVersion: '2021-11-15',
+      useCdn: true
+    }
+
+
+  )],
+ 
+});
