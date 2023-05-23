3363e1527d620def3976f9ed0fd13e645820674d
JUan Andrade <jfandradea@gmail.com>
2023-05-23 17:25:48 -0500

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import sanity from "astro-sanity";
 import svelte from '@astrojs/svelte';
 import nightOwl from './src/styles/night_owl.json';
-//import cobalt2 from './src/styles/cobalt2.json';
 
-import vercel from "@astrojs/vercel/serverless";
+
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
