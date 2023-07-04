c0a4689f5ba4067b26ed331766e22bc9f151db81
JUan Andrade <jfandradea@gmail.com>
2023-07-04 16:29:28 -0500

    set up blog with 3 first posts

--- src/util.js
+++ src/util.js
@@ -1,6 +1,22 @@
+import { getCollection } from "astro:content";
 export function formatDate(date) {  
 	return new Intl.DateTimeFormat('en-US', { dateStyle: 'long' })
 		.format(
 			new Date(date)
 		)
+}
+
+export function slugify(string) {
+  return string
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, '') // Remove non-word characters
+    .replace(/\s+/g, '-') // Replace whitespace with hyphens
+    .replace(/--+/g, '-') // Remove consecutive hyphens
+    .trim(); // Remove leading/trailing hyphens
+}
+
+export async function getBlogPosts() {
+	return (await getCollection("blog")).sort((a, b) => {
+		return a.data.pubDate.valueOf() - b.data.pubDate.valueOf();
+	});
 }
