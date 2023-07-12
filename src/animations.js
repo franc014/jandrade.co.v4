e16b918e7c5c39b7c8274723c4c2bd8a75011112
JUan Andrade <jfandradea@gmail.com>
2023-07-12 13:24:23 -0500

    improve animations

--- src/animations.js
+++ src/animations.js
@@ -6,9 +6,10 @@ export function triggerSetUp(element) {
     return {
         trigger: element,
         start: "top 85%",
-        end: "bottom 25%",
-        /* scrub: 1, */
-        toggleActions: "restart pause resume pause"
+        end: "bottom 55%",
+        
+        /* toggleActions: "play none resume reset", */
+       /*  markers: {startColor: "blue", endColor: "orange", fontSize: "20px"} */
     }
 }
 export function slideInline(element, moveX, rotation = 0) {
@@ -20,6 +21,34 @@ export function slideInline(element, moveX, rotation = 0) {
       transformOrigin: "50% 50%",
       ease: "sine",
       scrollTrigger: triggerSetUp(element)
+    });
+}
+export function slideInlineHighlight(element, moveX, rotation = 0) {
+  gsap.from(element, {
+      duration: 2,
+      x: moveX,
+      opacity: 0,
+      color: '#009FB7',
+      rotation,
+      transformOrigin: "50% 50%",
+      ease: "sine",
+      scrollTrigger: triggerSetUp(element)
+    });
+}
+
+export function slideInStagger(elements,moveY=400,rotation='-5deg') {
+  gsap.from(elements, {
+      duration: .8,
+      y: moveY,
+      opacity: 0,
+      rotation,
+      transformOrigin: "50% 50%",
+     
+      stagger: {
+        from: "center",
+        amount: 1,
+      },
+      scrollTrigger: triggerSetUp(elements)
     });
 }
 
